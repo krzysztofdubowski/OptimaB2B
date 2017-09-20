f129e2a400a58bd9485af4c9cb3a0fae032b7583
krzysztof dubowski <krzysztof.dubowski@gmail.com>
2017-09-20 12:31:10 +0200

    Dzialajacy Szablon
    Dzialajacy Szablon

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,48 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="home")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/starter.html.twig', [
+        return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+    
+     /**
+     * @Route("/zamowienia", name="zamowienia")
+     */
+    public function zamowieniaAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/zamowienia.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+    
+     /**
+     * @Route("/platnosci", name="platnosci")
+     */
+    public function platnosciAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/platnosci.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+    
+     /**
+     * @Route("/logowanie", name="logowanie")
+     */
+    public function LogowanieAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/LoginPage.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+    
+    
 }
