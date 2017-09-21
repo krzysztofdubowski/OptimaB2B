bc04e59c47af26f71a2a1f693fde29b170efd34d
krzysztof dubowski <krzysztof.dubowski@gmail.com>
2017-09-21 16:33:27 +0200

    poprawa

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -56,7 +56,7 @@ class DefaultController extends Controller
     public function DokumentAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/Dokument.html.twig', [
+        return $this->render('default/dokument.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
